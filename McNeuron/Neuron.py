f23a7a62199c8b42791115a3d1b7a32843960b56
RoozbehFarhoodi <roozbehfarhoudi@gmail.com>
2017-02-06 14:31:25 -0600

    update McNeuron for prufer

--- McNeuron/Neuron.py
+++ McNeuron/Neuron.py
@@ -8,6 +8,7 @@ from __builtin__ import str
 from copy import deepcopy
 import matplotlib.pyplot as plt
 from numpy.linalg import inv
+import data_transforms
 #np.random.seed(0)
 
 class Neuron(object):
@@ -96,7 +97,6 @@ class Neuron(object):
     """
 
     def __init__(self, file_format = None, input_file = None):
-
         """
         Default constructor. There are three ways of representing the neuron.
             In 'swc' the swc file in given and the output is a Neuron calss with all of its attributes.
@@ -119,7 +119,7 @@ class Neuron(object):
             self.set_parent()
             self.parent_index = self.parent_index.astype(int)
         if(file_format == 'only list of nodes'):
-            self.nodes_list =  input_file
+            self.nodes_list = input_file
             self.root = self.find_root(self.nodes_list[0])
             self.set_n_soma_n_node()
             self.set_parent()
@@ -131,10 +131,19 @@ class Neuron(object):
             self.read_swc_matrix(input_file)
             self.set_parent()
             self.parent_index = self.parent_index.astype(int)
-            self.set_parent()
-            self.parent_index = self.parent_index.astype(int)
-            #self.set_loc_diam()
-            self.fit()
+            self.set_branch_order()
+        if(file_format == 'prufer'):
+            # the prufer code and the location are given.
+            parents_code = data_transforms.decode_prufer(list(input_file[0].argmax(axis=1)))
+            location = input_file[1]
+            M = np.zeros([len(prufer), 7])
+            M[:,0] = np.arange(0, len(parents_code))
+            M[0,1] = 1
+            M[1:,1] = 2
+            M[1:,2:5] =  location
+            M[:,6] = parents_code
+            Neuron(file_format == 'Matrix of swc', input_file = M)
+
 
         #self.set_sholl()
 
@@ -619,350 +628,6 @@ class Neuron(object):
             return sum(self.distance_from_parent[I]) - self.distance_from_parent[Upindex]
         else:
             return np.inf
-
-    def get_main_points(self):
-        """
-        gets the index of branching points and end points.
-        """
-        (branch_index,) = np.where(self.branch_order[self.n_soma:]==2)
-        (endpoint_index,) = np.where(self.branch_order[self.n_soma:]==0)
-        selected_index = np.union1d(branch_index + self.n_soma, endpoint_index + self.n_soma)
-        selected_index = np.append(range(self.n_soma), selected_index)
-        return selected_index
-
-    def parent_id(self, selected_index):
-        """
-        Return the parent id of all the selected_index of the neurons.
-        Parameters
-        ----------
-        selected_index: numpy array
-            the index of nodes
-        Returns
-        -------
-        parent_id: the index of parent of each element in selected_index in this
-        array.
-        """
-        parent_id = np.array([], dtype=int)
-        for i in selected_index:
-            p = self.parent_index[i]
-            while(~np.any(selected_index == p)):
-                p = self.parent_index[p]
-            (ind,) = np.where(selected_index == p)
-            parent_id = np.append(parent_id, ind)
-        return parent_id
-
-    def neuron_with_selected_nodes(self, selected_index):
-        """
-        Gives back a new neuron made up with the selected_index nodes of self.
-        if node A is parent (or grand parent) of node B in the original neuron, it is the same for the new neuron.
-        Parameters
-        ----------
-        selected_index: numpy array
-            the index of nodes from original neuron for making new neuron
-        Returns
-        -------
-        Neuron: the subsampled neuron
-        """
-        parent = self.parent_id(selected_index)
-        # making the list of nodes
-        n_list = []
-        for i in range(selected_index.shape[0]):
-            n = Node()
-            n.xyz = self.nodes_list[selected_index[i]].xyz
-            n.r = self.nodes_list[selected_index[i]].r
-            n.type = self.nodes_list[selected_index[i]].type
-            n_list.append(n)
-        # adjusting the childern and parents for the nodes.
-        for i in np.arange(1, selected_index.shape[0]):
-            j = parent[i]
-            n_list[i].parent = n_list[j]
-            n_list[j].add_child(n_list[i])
-        return Neuron(file_format = 'only list of nodes', input_file = n_list)
-
-    def find_sharpest_fork(self, Nodes):
-        """
-        Looks at the all branching point in the Nodes list, selects those which both its children are end points and finds
-        the closest pair of childern (the distance between children).
-        Parameters
-        ----------
-        Nodes: list
-        the list of Node
-
-        Returns
-        -------
-        sharpest_pair: array
-            the index of the pair of closest pair of childern
-        distance: float
-            Distance of the pair of children
-        """
-        pair_list = []
-        Dis = np.array([])
-        for n in Nodes:
-            if n.parent is not None:
-                if n.parent.parent is not None:
-                    a = n.parent.children
-                    if(isinstance(a, list)):
-                        if(len(a)==2):
-                            n1 = a[0]
-                            n2 = a[1]
-                            if(len(n1.children) == 0 and len(n2.children) == 0):
-                                pair_list.append([n1 , n2])
-                                dis = LA.norm(a[0].xyz - a[1].xyz,2)
-                                Dis = np.append(Dis,dis)
-        if(len(Dis)!= 0):
-            (b,) = np.where(Dis == Dis.min())
-            sharpest_pair = pair_list[b[0]]
-            distance = Dis.min()
-        else:
-            sharpest_pair = [0,0]
-            distance = 0.
-        return sharpest_pair, distance
-
-    def find_sharpest_fork_general(self, Nodes):
-        """
-        Looks at the all branching point in the Nodes list, selects those which both its children are end points and finds
-        the closest pair of childern (the distance between children).
-        Parameters
-        ----------
-        Nodes: list
-        the list of Node
-
-        Returns
-        -------
-        sharpest_pair: array
-            the index of the pair of closest pair of childern
-        distance: float
-            Distance of the pair of children
-        """
-        pair_list = []
-        Dis = np.array([])
-        for n in Nodes:
-            if n.parent is not None:
-                if n.parent.parent is not None:
-                    a = n.parent.children
-                    if(isinstance(a, list)):
-                        if(len(a)==2):
-                            n1 = a[0]
-                            n2 = a[1]
-                            pair_list.append([n1 , n2])
-                            dis = LA.norm(a[0].xyz - a[1].xyz,2)
-                            Dis = np.append(Dis,dis)
-        if(len(Dis)!= 0):
-            (b,) = np.where(Dis == Dis.min())
-            sharpest_pair = pair_list[b[0]]
-            distance = Dis.min()
-        else:
-            sharpest_pair = [0,0]
-            distance = 0.
-        return sharpest_pair, distance
-
-    def remove_pair_replace_node(self, Nodes, pair):
-        """
-        Removes the pair of nodes and replace it with a new node. the parent of new node is the parent of the pair of node,
-        and its location and its radius are the mean of removed nodes.
-        Parameters
-        ----------
-        Nodes: list
-        the list of Nodes
-
-        pair: array
-        The index of pair of nodes. the nodes should be end points and have the same parent.
-
-        Returns
-        -------
-        The new list of Nodes which the pair are removed and a mean node is replaced.
-        """
-
-        par = pair[0].parent
-        loc = pair[0].xyz + pair[1].xyz
-        loc = loc/2
-        r = pair[0].r + pair[1].r
-        r = r/2
-        Nodes.remove(pair[1])
-        Nodes.remove(pair[0])
-        n = Node()
-        n.xyz = loc
-        n.r = r
-        par.children = []
-        par.add_child(n)
-        n.parent = par
-        Nodes.append(n)
-
-    def remove_pair_adjust_parent(self, Nodes, pair):
-        """
-        Removes the pair of nodes and adjust its parent. the location of the parent is the mean of the locaton of two nodes.
-
-        Parameters
-        ----------
-        Nodes: list
-        the list of Nodes
-
-        pair: array
-        The index of pair of nodes. the nodes should be end points and have the same parent.
-
-        Returns
-        -------
-        The new list of Nodes which the pair are removed their parent is adjusted.
-        """
-
-        par = pair[0].parent
-        loc = pair[0].xyz + pair[1].xyz
-        loc = loc/2
-        Nodes.remove(pair[1])
-        Nodes.remove(pair[0])
-        par.xyz = loc
-        par.children = []
-
-    def prune_shortest_seg(self):
-        (endpoint_index,) = np.where(self.branch_order[self.n_soma:]==0)
-        #for i in endpoint_index:
-
-
-
-    def random_subsample(self, num):
-        """
-        randomly selects a few nodes from neuron and builds a new neuron with them. The location of these node in the new neuron
-        is the same as the original neuron and the morphology of them is such that if node A is parent (or grand parent) of node B
-        in the original neuron, it is the same for the new neuron.
-
-        Parameters
-        ----------
-        num: int
-            number of nodes to be selected randomly.
-
-        Returns
-        -------
-        Neuron: the subsampled neuron
-        """
-
-        # select the index of num nodes randomly.
-        I = np.arange(self.n_soma, self.n_node)
-        np.random.shuffle(I)
-        selected_index = I[0:num]
-        selected_index = np.union1d(np.arange(self.n_soma), selected_index)
-        selected_index = selected_index.astype(int)
-        selected_index = np.unique(np.sort(selected_index))
-
-        # making a list of node from the selected nodes
-        neuron = self.neuron_with_selected_nodes(selected_index)
-
-        return neuron
-
-    def regular_subsample(self):
-        """
-        subsamples a neuron with its main node only; i.e endpoints and branching nodes.
-
-        Returns
-        -------
-        Neuron: the subsampled neuron
-        """
-        # select all the main points
-        selected_index = self.get_main_points()
-
-        # Computing the parent id of the selected nodes
-        neuron = self.neuron_with_selected_nodes(selected_index)
-        return neuron
-
-    def straigh_subsample(self, distance):
-        """
-        Subsamples a neuron from original neuron. It has all the main points of the original neuron,
-        i.e endpoints or branching nodes, are not changed and meanwhile the distance of two consecutive nodes
-        of subsample neuron is around the 'distance'.
-        for each segment between two consecuative main points, a few nodes from the segment will be added to the selected node;
-        it starts from the far main point, and goes on the segment toward the near main point. Then the first node which is
-        going to add has the property that it is the farest node from begining on the segment such that its distance from begining is
-        less than 'distance'. The next nodes will be selected similarly. this procesure repeat for all the segments.
-
-        Parameters
-        ----------
-        distance: float
-            the mean distance between pairs of consecuative nodes.
-
-        Returns
-        -------
-        Neuron: the subsampled neuron
-        """
-
-        # Selecting the main points: branching nodes and end nodes
-        selected_index = self.get_main_points()
-
-        # for each segment between two consecuative main points, a few nodes from the segment will be added to the selected node.
-        # These new nodes will be selected base on the fact that neural distance of two consecuative nodes is around 'distance'.
-        # Specifically, it starts from the far main point, and goes on the segment toward the near main point. Then the first node which is
-        # going to add has the property that it is the farest node from begining on the segment such that its distance from begining is
-        # less than 'distance'. The next nodes will be selected similarly.
-
-        for i in selected_index:
-            upList = np.array([i],dtype = int)
-            index = self.parent_index[i]
-            dist = self.distance_from_parent[i]
-            while(~np.any(selected_index == index)):
-                upList = np.append(upList,index)
-                index = self.parent_index[index]
-                dist = np.append(dist, sum(self.distance_from_parent[upList]))
-            dist = np.append(0,dist)
-            (I,) = np.where(np.diff(np.floor(dist/distance))>0)
-            I = upList[I]
-            selected_index = np.append(selected_index,I)
-        selected_index = np.unique(selected_index)
-
-        neuron = self.neuron_with_selected_nodes(selected_index)
-
-        return neuron
-
-    def straight_subsample_with_fixed_number(self, num):
-        """
-        gives back a regular subsample neuron (regular means that the distance between consecuative nodes is approximately fixed)
-        such that the number of nodes is 'num'.
-
-        Parameters
-        ----------
-        num: int
-            number of nodes on the subsampled neuron
-
-        Returns
-        -------
-        Neuron: the subsampled neuron
-
-        """
-        l = sum(self.distance_from_parent)
-        branch_number = len(np.where(self.branch_order[self.n_soma:] == 2))
-        distance = l/(num - branch_number)
-        return self.straigh_subsample(distance)
-
-    def prune_subsample(self, number):
-        main_point = self.subsample_main_nodes()
-        Nodes = main_point.nodes_list
-        rm = (main_point.n_node - number)/2.
-        for remove in range(int(rm)):
-            b, m = self.find_sharpest_fork(Nodes)
-            self.remove_pair_adjust_parent(Nodes, b)
-
-        return Neuron(file_format = 'only list of nodes', input_file = Nodes)
-
-    def straight_prune_subsample(self, number):
-        thresh = 1.
-    #     n = neuron.subsample(thresh)
-    #     while(len(n.nodes_list)>number):
-    #         thresh += 1
-    #         n = neuron.subsample(thresh)
-    #         if(sum(n.branch_order[n.n_soma:]==1)==0):
-    #             break
-    #     neuron = n
-        Nodes = self.nodes_list
-        while(len(Nodes) > number):
-            b, m = self.find_sharpest_fork_general(Nodes)
-            print m
-            if(m > 0. and m < thresh):
-                self.remove_pair_replace_node(Nodes, b)
-            else:
-                self = Neuron(file_format = 'only list of nodes', input_file = Nodes)
-                thresh = thresh + 1
-                self = self.subsample(thresh)
-                Nodes = self.nodes_list
-                print thresh
-
-        return Neuron(file_format = 'only list of nodes', input_file = Nodes)
 
     def calculate_overall_matrix(self, node):
         j = self.get_index_for_no_soma_node(node)
@@ -1781,6 +1446,7 @@ class Neuron(object):
         except:
             print('deleted Neuron')
 
+
     def read_swc_matrix(self, input_file):
         """
         Read the an swc matrix and fill the attributes accordingly.
@@ -1843,6 +1509,7 @@ class Neuron(object):
         a = a.toarray()
         a[a==0] = np.nan
         self.child_index = a
+
 
     def get_swc(self):
         swc = np.zeros([self.n_node,7])
